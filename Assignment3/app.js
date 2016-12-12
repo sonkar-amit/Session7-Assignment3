62a4fc66785a8c58c17c795c5606dd7f60636060
sonkar-amit <sonkar.amit08@gmail.com>
2016-12-12 13:07:18 +0530

    JS3 Assignment3

---
+++ Assignment3/app.js
@@ -0,0 +1,12 @@
+var increment = 0;
+
+function counter()
+{
+    return ++increment;
+}
+
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
